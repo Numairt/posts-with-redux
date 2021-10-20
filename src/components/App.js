c8c532fa64b8490309a5c217e8dd20b0efda0c04
Numair <59014011+Numairt@users.noreply.github.com>
2021-10-20 23:39:13 +0500

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostList from './PostList';
+
+const App = () => {
+    return(
+        <div className="container">
+            <PostList/>
+        </div>
+        
+    );
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+    dummy: () => 'change it in future'
+});
